0cd79dceb2d14c5d65a2bf9aaff5bec3b277a7e0
nariman <nariman.wk@gmail.com>
2015-12-10 12:36:13 +0300

    slider work

--- js/custom-develop.js
+++ js/custom-develop.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
+/*
+##############################
+- ACTIVATE THE BANNER HERE -
+##############################
+*/
 
+    $('#banner0').owlCarousel({
+        items: 6,
+        autoPlay: 3000,
+        singleItem: true,
+        navigation: false,
+        pagination: false,
+        transitionStyle: 'fade'
+    });
 });
